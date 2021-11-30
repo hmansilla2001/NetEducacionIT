d3f8e38d5ef79565128a81387d5f253493128979
ramiromurua08 <ramiromurua08@outlook.com>
2021-11-30 20:35:38 -0300

    Agregado

---
+++ MayoristaEstrella/Bussiness/MarcaBussiness.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using DAL;
+using Entidades;
+
+namespace Bussiness
+{
+    public class MarcaBussiness
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["LibraryDBConnectionString"].ConnectionString;
+        MarcaDAL MarcaDal = new MarcaDAL();
+        public List<Marca> GetMarcas()
+        {
+            return MarcaDal.GetMarcas(connectionString);
+        }
+
+        public void CrearMarca(Marca marca)
+        {
+            MarcaDal.CrearMarca(connectionString, marca);
+        }
+
+        public Marca GetMarcaData(int MarcaId)
+        {
+            return MarcaDal.GetMarcaData(connectionString, MarcaId);
+        }
+
+        public void EditarMarca(Marca marca)
+        {
+            MarcaDal.EditarMarca(connectionString, marca);
+        }
+
+        public void DeleteMarca(int MarcaId)
+        {
+            MarcaDal.DeleteMarca(connectionString, MarcaId);
+        }
+    }
+}
